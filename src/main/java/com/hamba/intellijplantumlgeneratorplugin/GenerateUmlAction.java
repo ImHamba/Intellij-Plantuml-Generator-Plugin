c4c8a6593278c83d62b175972a43168986e04cc9
ImHamba <callmehamba@gmail.com>
2023-04-01 01:24:36 +1100

    set up GenerateUmlAction to use PsiClass processor on all project classes

--- src/main/java/com/hamba/intellijplantumlgeneratorplugin/GenerateUmlAction.java
+++ src/main/java/com/hamba/intellijplantumlgeneratorplugin/GenerateUmlAction.java
@@ -1,15 +1,39 @@
 package com.hamba.intellijplantumlgeneratorplugin;
 
+import com.intellij.codeInsight.completion.AllClassesGetter;
+import com.intellij.codeInsight.completion.PlainPrefixMatcher;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.PsiClass;
+import com.intellij.util.Processor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Map;
 
 public class GenerateUmlAction extends AnAction {
+    Map<PsiType, List<PsiType>> associations;
+    Map<PsiType, List<PsiType>> dependencies;
+    Map<PsiType, List<PsiType>> interfaces;
+    Map<PsiType, List<PsiType>> extensions;
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        PsiFile file = e.getData(CommonDataKeys.PSI_FILE);
+        Project project = e.getProject();
+        ClassProcessor processor = new ClassProcessor();
+
+        // searches for classes with '' prefix (i.e. all classes) in the project and runs class processor on them
+        AllClassesGetter.processJavaClasses(
+            new PlainPrefixMatcher(""),
+            project,
+            GlobalSearchScope.projectScope(project),
+            processor);
     }
 
     @Override
