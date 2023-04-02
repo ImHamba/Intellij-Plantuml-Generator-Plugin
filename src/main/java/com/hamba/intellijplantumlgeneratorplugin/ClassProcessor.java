184479f1f8dc2fa09adb5707927219785ddf1467
ImHamba <callmehamba@gmail.com>
2023-04-02 14:51:24 +1000

    refactored ClassProcessor to separately gather all classes and then process them to generate class relationships

--- src/main/java/com/hamba/intellijplantumlgeneratorplugin/ClassProcessor.java
+++ src/main/java/com/hamba/intellijplantumlgeneratorplugin/ClassProcessor.java
@@ -2,46 +2,89 @@ package com.hamba.intellijplantumlgeneratorplugin;
 
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiField;
-import com.intellij.psi.PsiType;
 import com.intellij.psi.util.PsiUtil;
 import com.intellij.util.Processor;
 
 import java.util.*;
 
 public class ClassProcessor implements Processor<PsiClass> {
-    // stores the set of relationships between each class and other classes
-    private Map<PsiClass, List<PsiClass>> allAssociations = new HashMap<>();
-    private Map<PsiClass, List<PsiClass>> allDependencies = new HashMap<>();
-    private Map<PsiClass, List<PsiClass>> allInterfaces = new HashMap<>();
-    private Map<PsiClass, PsiClass> allExtensions = new HashMap<>();
+
+    private List<PsiClass> allClasses = new ArrayList<>();
 
     // processor that is used below on each class PsiClass object
     @Override
     public boolean process(PsiClass currentClass) {
-        // stores the set of relationships between the current class being processed and other classes
-        List<PsiClass> associations = processAssociations(currentClass);
-        List<PsiClass> dependencies = processDependencies(currentClass);
-        List<PsiClass> interfaces = processInterfaces(currentClass);
-        PsiClass extension = processExtensions(currentClass);
-
-        // add current class relationships to maps holding all relationships
-        if (associations.size() > 0) {
-            allAssociations.put(currentClass, associations);
-        }
-
-        if (interfaces.size() > 0) {
-            allInterfaces.put(currentClass, interfaces);
-        }
-
-        if (dependencies.size() > 0) {
-            allDependencies.put(currentClass, dependencies);
-        }
-
-        if (extension != null) {
-            allExtensions.put(currentClass, extension);
-        }
-
+        allClasses.add(currentClass);
         return true;
+    }
+
+    public Map<PsiClass, List<PsiClass>> generateAssociations() {
+        // stores the set of relationships between each class and other classes
+        Map<PsiClass, List<PsiClass>> allAssociations = new HashMap<>();
+
+        // iterate through each class in the project
+        for (PsiClass currentClass : allClasses) {
+            // stores the set of relationships between the current class being processed and other classes
+            List<PsiClass> associations = processAssociations(currentClass);
+
+            // add current class relationships to maps holding all relationships
+            if (associations.size() > 0) {
+                allAssociations.put(currentClass, associations);
+            }
+        }
+
+        return allAssociations;
+    }
+
+    public Map<PsiClass, List<PsiClass>> generateDependencies() {
+        // stores the set of relationships between each class and other classes
+        Map<PsiClass, List<PsiClass>> allDependencies = new HashMap<>();
+
+        // iterate through each class in the project
+        for (PsiClass currentClass : allClasses) {
+            // stores the set of relationships between the current class being processed and other classes
+            List<PsiClass> dependencies = processDependencies(currentClass);
+
+            if (dependencies.size() > 0) {
+                allDependencies.put(currentClass, dependencies);
+            }
+        }
+
+        return allDependencies;
+    }
+
+    public Map<PsiClass, List<PsiClass>> generateInterfaces() {
+        // stores the set of relationships between each class and other classes
+        Map<PsiClass, List<PsiClass>> allInterfaces = new HashMap<>();
+
+        // iterate through each class in the project
+        for (PsiClass currentClass : allClasses) {
+            // stores the set of relationships between the current class being processed and other classes
+            List<PsiClass> interfaces = processInterfaces(currentClass);
+
+            if (interfaces.size() > 0) {
+                allInterfaces.put(currentClass, interfaces);
+            }
+        }
+
+        return allInterfaces;
+    }
+
+    public Map<PsiClass, PsiClass> generateInheritances() {
+        // stores the set of relationships between each class and other classes
+        Map<PsiClass, PsiClass> allInheritances = new HashMap<>();
+
+        // iterate through each class in the project
+        for (PsiClass currentClass : allClasses) {
+            // stores the set of relationships between the current class being processed and other classes
+            PsiClass inheritances = processInheritances(currentClass);
+
+            if (inheritances != null) {
+                allInheritances.put(currentClass, inheritances);
+            }
+        }
+
+        return allInheritances;
     }
 
     public List<PsiClass> processAssociations(PsiClass currentClass) {
@@ -68,12 +111,19 @@ public class ClassProcessor implements Processor<PsiClass> {
     }
 
     public List<PsiClass> processDependencies(PsiClass currentClass) {
+        // TODO: implement this
         return new ArrayList<>();
     }
 
-    public PsiClass processExtensions(PsiClass currentClass) {
+    public PsiClass processInheritances(PsiClass currentClass) {
         // get class this class extends from
         // returns null if current class does not extend anything
-        return currentClass.getSuperClass();
+        PsiClass parent = currentClass.getSuperClass();
+
+        if (allClasses.contains(parent)) {
+            return parent;
+        }
+
+        return null;
     }
 }
