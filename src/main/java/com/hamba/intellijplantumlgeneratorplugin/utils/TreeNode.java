f3d1c297830c37eb7740b7363b6a58fbdf8b7bbe
ImHamba <callmehamba@gmail.com>
2023-04-02 16:16:00 +1000

    added getChildren getter to TreeNode

--- src/main/java/com/hamba/intellijplantumlgeneratorplugin/utils/TreeNode.java
+++ src/main/java/com/hamba/intellijplantumlgeneratorplugin/utils/TreeNode.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public class TreeNode {
     private List<TreeNode> children;
+
     private String name;
     private TreeNode parent;
-
     public TreeNode(String name, TreeNode parent) {
         this.name = name;
         this.parent = parent;
@@ -14,6 +14,10 @@ public class TreeNode {
 
     public void addChild(TreeNode newChild) {
         children.add(newChild);
+    }
+
+    public List<TreeNode> getChildren() {
+        return children;
     }
 
 }
