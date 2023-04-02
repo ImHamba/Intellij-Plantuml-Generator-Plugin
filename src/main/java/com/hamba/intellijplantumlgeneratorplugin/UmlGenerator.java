9f3160d628236b03dc283d8b73d935991a473377
ImHamba <callmehamba@gmail.com>
2023-04-02 16:02:30 +1000

    added unfinished class for generating uml syntax

---
+++ src/main/java/com/hamba/intellijplantumlgeneratorplugin/UmlGenerator.java
@@ -0,0 +1,18 @@
+package com.hamba.intellijplantumlgeneratorplugin;
+
+import com.intellij.psi.PsiClass;
+
+import java.util.List;
+import java.util.Map;
+
+public class UmlGenerator {
+    /**
+     * Generates UML syntax from a map of class relationships
+     * @return
+     */
+    static String umlRelationshipGenerator(Map<PsiClass, List<PsiClass>> classRelationships) {
+
+
+        return null;
+    }
+}
