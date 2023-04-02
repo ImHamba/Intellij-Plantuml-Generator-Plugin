4bd3197c6bbe4391703bc351902be1104dde9865
ImHamba <callmehamba@gmail.com>
2023-04-02 16:08:11 +1000

    implemented TreeNode

--- src/main/java/com/hamba/intellijplantumlgeneratorplugin/utils/TreeNode.java
+++ src/main/java/com/hamba/intellijplantumlgeneratorplugin/utils/TreeNode.java
@@ -1,4 +1,19 @@
 package com.hamba.intellijplantumlgeneratorplugin.utils;
 
+import java.util.List;
+
 public class TreeNode {
+    private List<TreeNode> children;
+    private String name;
+    private TreeNode parent;
+
+    public TreeNode(String name, TreeNode parent) {
+        this.name = name;
+        this.parent = parent;
+    }
+
+    public void addChild(TreeNode newChild) {
+        children.add(newChild);
+    }
+
 }
